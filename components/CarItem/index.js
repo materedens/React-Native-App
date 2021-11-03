e29099a78d1a479a3b0608850dd349c022774cd0
materedens <denismatere@gmail.com>
2021-11-03 11:22:55 +0300

    React-native-UI-App

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import {View, Text, ImageBackground} from 'react-native';
+import StyledButton from '../StyledButton';
 import styles from './styles';
 
 const CarItem = (props) => {
 
+    const{name, tagline, taglineCTA, image} = props.car;
+
   return (
     <View style={styles.carContainer}>
       <ImageBackground
-        source={require('../../assets/images/ModelX.jpeg')}
+        source={image}
         style={styles.image}
       />
 
       <View style={styles.titles}>
         <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at Kshs. 6.9K</Text>
+        <Text style={styles.subtitle}>
+            {tagline}{''}</Text>
+            <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
       </View>
+
+      <View style={styles.buttonsContainer}>
+          <StyledButton type="primary" content={"Custom Order"} onPress={() => {
+               console.warn("Custom Order was pressed");
+         }} />
+
+          <StyledButton type="secondary" content={"Existing Inventory"} onPress={() => {
+          console.warn("Existing Inventory was pressed");
+         }} />
+      </View>
+      
     </View>
   );
 };
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles= StyleSheet.create({
     carContainer: {
         width:'100%',
-        height:'100%',
+        height: Dimensions.get('window').height,
     
       },
       titles:{
@@ -17,6 +17,10 @@ const styles= StyleSheet.create({
         fontWeight:'500',
     
       },
+      subtitleCTA:{
+        textDecorationLine:'underline',
+
+      },
       subtitle:{
         fontSize: 16,
         color:'#5c5e62',
@@ -26,6 +30,11 @@ const styles= StyleSheet.create({
         height:'100%',
         resizeMode:'cover',
         position:'absolute',
+      },
+      buttonsContainer:{
+          position: 'absolute',
+          bottom: 50,
+          width: '100%',
       }
 });
 
